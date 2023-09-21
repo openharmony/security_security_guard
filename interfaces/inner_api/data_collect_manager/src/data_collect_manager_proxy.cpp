50bbbead6a4ff9c9a1bbfd1b91e840b2828596a9
zhaoguojian <zhaoguojian@huawei.com>
2023-09-21 20:48:28 +0800

    流水线环境搭建
    Signed-off-by: zhaoguojian <zhaoguojian@huawei.com>

--- interfaces/inner_api/data_collect_manager/src/data_collect_manager_proxy.cpp
+++ interfaces/inner_api/data_collect_manager/src/data_collect_manager_proxy.cpp
@@ -27,7 +27,7 @@ DataCollectManagerProxy::DataCollectManagerProxy(const sptr<IRemoteObject> &impl
 int32_t DataCollectManagerProxy::RequestDataSubmit(int64_t eventId, std::string &version,
     std::string &time, std::string &content)
 {
-    SGLOGD("eventId=%{public}ld, version=%{public}s", eventId, version.c_str());
+    SGLOGD("eventId=%{public}lld, version=%{public}s", static_cast<long long>(eventId), version.c_str());
     MessageParcel data;
     MessageParcel reply;
     if (!data.WriteInterfaceToken(GetDescriptor())) {
