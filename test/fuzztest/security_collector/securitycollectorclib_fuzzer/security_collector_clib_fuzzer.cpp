50ac88861967e0e3054a739c60c5c6dacea2bfdd
kcxm <zhangzhaohua2@huawei.com>
2025-12-29 11:57:47 +0800

    fix Fuzz Signed-off-by: zzh <zhangzhaohua2@huawei.com>
    Change-Id: Ifc97a6d56bd8b4a81314b450671dd7eab2eed076

--- test/fuzztest/security_collector/securitycollectorclib_fuzzer/security_collector_clib_fuzzer.cpp
+++ test/fuzztest/security_collector/securitycollectorclib_fuzzer/security_collector_clib_fuzzer.cpp
@@ -141,5 +141,6 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
     /* Run your code on date */
     FuzzedDataProvider fdp(data, size);
     FuzzSecurityCollector(fdp);
+    g_service.OnStop();
     return 0;
 }
